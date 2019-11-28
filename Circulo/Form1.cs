3d39fbb96b035db0b053e231ea6254c7dd46057d
Brunodesa <49911022+Brunodesa@users.noreply.github.com>
2019-11-28 10:53:29 +0000

    Add files via upload

---
+++ Circulo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Redondo r = new Redondo();
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            r.Raio = textBox1.Text;
+            if (radioButton1.Checked) label1.Text = r.Area;
+            else if (radioButton2.Checked) label1.Text = r.Perimetro;
+            else if (radioButton3.Checked) label1.Text = r.Diametro;
+        }
+    }
+}
---
+++ Circulo/Redondo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    class Redondo
+    {
+        private double _r;
+        public string Raio { set => _r = Convert.ToDouble(value); }
+        public string Area => (Math.PI *(_r * _r)).ToString();
+        public string Perimetro => ((2 * Math.PI) * _r).ToString();
+        public string Diametro => (_r * 2).ToString();
+    }
+}
